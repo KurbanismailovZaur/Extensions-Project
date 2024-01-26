c1d49246d32e15fac24a1a22a5647dacc823f95d
Zaur Kurbanismailov <kurbanismailov.92@gmail.com>
2024-01-26 18:33:26 +0300

    Update submodule

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -1,14 +1,12 @@
 using System;
+using System.Collections.Generic;
 using Codomaster.Extensions;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace DefaultNamespace
 {
     public class Test : MonoBehaviour
     {
-        private void Start()
-        {
-            var max = Vector3.forward.MaxComponent();
-        }
     }
 }
